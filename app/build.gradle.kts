f5077db852ea45e4bae84ed070a4ac989b89b8c6
SoClear <48470201+SoClear@users.noreply.github.com>
2026-03-09 23:00:01 +0800

    chore: bump version to 1.0.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "io.github.soclear.oneuix"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         ndk {
             // ABI过滤，只加载 arm 架构 64 位动态链接库
